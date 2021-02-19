002e46bf798cd4b5d53f68c25125c3be49239f2a
unknown <mukulpalekar@gmail.com>
2021-02-20 01:09:11 +0530

    test2

---
+++ Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string user_id = TextBox1.Text;
+        string pwd = TextBox2.Text;
+        Service db = new Service();
+        try
+        {
+            if (db.login_validate(user_id, pwd))
+            {
+                Label1.Text = "Login success";
+            }
+            else { Label1.Text = "Login Fail"; }
+        }
+        catch (Exception ex) { Label1.Text = "Error - "+ex.Message; }
+    }
+}
