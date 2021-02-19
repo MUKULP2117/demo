a2d1ea17096adbe441b044396f0b972b764b97bf
unknown <mukulpalekar@gmail.com>
2021-02-20 00:47:42 +0530

    test2

---
+++ App_Code/DB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// Summary description for DB
+/// </summary>
+public class DB
+{
+    public DB()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+
+       
+    }
+
+    public bool login_Validate(string userid, string pwd,string db_File)
+    {
+        bool is_valid = false;
+        string qry = "select count(*) from users where user_id = '"+userid+"' and pwd = '"+pwd+"'";
+        DataTable dt = get_dt(qry,db_File);
+        if (dt != null && dt.Rows.Count > 0)
+        {
+            if (Convert.ToInt32(dt.Rows[0][0].ToString()) > 0)
+            {
+                is_valid = true;
+            }
+        }
+        return is_valid;
+
+    }
+
+    public DataTable get_dt(string qry,string db_File)
+    {
+        DataTable dt = new DataTable();
+        try
+        {
+            string sqlCon = @"Data Source=.\SQLEXPRESS; AttachDbFilename=" + db_File + "; Integrated Security=True; Connect Timeout=30; User Instance=True";
+            SqlConnection Con = new SqlConnection(sqlCon);
+            SqlDataAdapter da = new SqlDataAdapter(qry, Con);
+            da.Fill(dt);
+        }
+        catch(Exception ex) { }
+        return dt;
+
+    }
+}
---
+++ App_Code/Service.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for Service
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+ [System.Web.Script.Services.ScriptService]
+public class Service : System.Web.Services.WebService
+{
+
+    public Service()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public bool login_validate(string userid, string pwd)
+    {
+        string db_file = Server.MapPath("~/App_Data/Database.mdf");
+        bool is_Validate = false;
+        DB _db = new DB();
+        is_Validate =_db.login_Validate(userid, pwd, db_file);
+        return is_Validate;
+    }
+
+}
